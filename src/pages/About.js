316cf44496efb43350960c67a55bfc3a1879925b
PtitLudo64 <ludovic.p.777@gmail.com>
2022-05-23 12:41:16 +0200

    Init du projet

---
+++ src/pages/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div >
+            <Navigation></Navigation>
+            <h1>A propos</h1>
+            <br />
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus rerum eius est sed accusantium. Tenetur aliquam eligendi sequi quia, recusandae pariatur expedita iusto quis reprehenderit veniam voluptas, perferendis porro nesciunt beatae quam natus consequatur? Commodi, ducimus consequuntur saepe perferendis reprehenderit odio aperiam atque illum qui veritatis consequatur est unde officia cumque. Incidunt molestias corporis fuga, ipsam cum reprehenderit expedita delectus atque illum est magnam molestiae possimus explicabo esse minima repudiandae. Facilis aliquid magni temporibus hic amet distinctio non beatae eligendi optio eius quas blanditiis ut nam nihil aut molestias ex assumenda doloremque, fugit mollitia quae ab enim. Veritatis rerum ducimus eveniet fuga non expedita in vel maxime, necessitatibus distinctio illum ad nisi reiciendis dolor dolore earum delectus amet hic quae odio. Nesciunt atque recusandae ea eaque nisi neque optio, voluptates dolorum hic cumque similique consequuntur consequatur aut iure non vero ipsum alias labore ipsam voluptatem est id ducimus enim. Soluta, mollitia laudantium dolorum dolorem nesciunt iure reprehenderit minima, voluptatum perferendis corrupti veniam tempore ipsam architecto quae! Iusto iste non voluptas accusantium sapiente ullam quas, molestias reiciendis adipisci nemo exercitationem quia sequi molestiae repudiandae alias fugiat quaerat, dignissimos in recusandae omnis assumenda. Inventore, tenetur quam? Ea eum quam tenetur illum eaque.
+            </p>
+            <br />
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci sed quae asperiores nihil vero molestias ipsa! Non ad sint quisquam distinctio et, fuga tempore vitae, quidem architecto, reiciendis repellat quas unde sequi molestias fugit. Nostrum molestias deserunt iste cumque, dolorum temporibus quo. Ea expedita eum ipsum delectus magni. Dolorem temporibus excepturi, in expedita exercitationem distinctio, reprehenderit iste voluptas ullam cupiditate illo saepe porro tenetur dicta odit voluptatibus vero? Minus sit est quia praesentium ipsum pariatur harum aperiam delectus perferendis, mollitia voluptatum dicta, vitae ad et beatae quam dignissimos reprehenderit ut? Earum provident esse ut fuga commodi, optio minus! Assumenda, quaerat.
+            </p>
+        </div>
+    );
+};
+
+export default About;
